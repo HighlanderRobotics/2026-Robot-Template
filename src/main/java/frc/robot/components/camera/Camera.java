6d10684db17be923f4229afead4ab6950d76a07b
spellingcat <70864274+spellingcat@users.noreply.github.com>
2025-12-04 15:31:37 -0800

    add some vision comments

--- src/main/java/frc/robot/components/camera/Camera.java
+++ src/main/java/frc/robot/components/camera/Camera.java
@@ -39,8 +39,14 @@ public class Camera {
       Matrix<N3, N3> intrinsicsMatrix,
       Matrix<N8, N1> distCoeffs) {}
 
+  // These standard deviations are used for scaling how much we "trust" a pose estimate
+  // Standard deviations are a way of measuring how far away something is from the mean. A dataset with a very high standard deviation is very spread out, versus a dataset with a low standard deviation has most of its data clustered around the mean
+  // In other words, the higher the standard deviation, the greater the noise
+  // Noisy estimates are less reliable, so we can increase or decrease these standard deviation values associated with a pose estimate to reflect how reliable we think an estimate is
   public static final Matrix<N3, N1> visionPointBlankDevs =
       new Matrix<N3, N1>(Nat.N3(), Nat.N1(), new double[] {0.6, 0.6, 0.5});
+  
+  // infinite devs are used to show that we really don't trust this estimate
   public static final Matrix<N3, N1> infiniteDevs =
       new Matrix<N3, N1>(
           Nat.N3(),
@@ -58,19 +64,17 @@ public class Camera {
           PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
           null);
 
-  private Alert futureVisionData;
+  // set up error alerts
+  private final Alert futureVisionData = new Alert(getName() + " Vision Data Coming from ✨The Future✨", AlertType.kError);
   private final Alert disconnectedAlert = new Alert(getName() + " Disconnected!", AlertType.kError);
 
   private Pose3d pose;
 
   public Camera(CameraIO io) {
     this.io = io;
+    // Tells the estimator what the transformation is between the camera and the robot
     estimator.setRobotToCameraTransform(io.getCameraConstants().robotToCamera);
     io.updateInputs(inputs);
-    futureVisionData =
-        new Alert(
-            getName() + " Vision Data Coming from ✨The Future✨",
-            AlertType.kError); // what the hell is this
   }
 
   // MUST CALL FROM SUBSYSTEM! NOT PART OF COMMAND SCHEDULER
@@ -91,7 +95,8 @@ public class Camera {
   }
 
   public Optional<EstimatedRobotPose> update(PhotonPipelineResult result) {
-    // Skip if we have no targets (could/should switch to 1?)
+
+    //if we don't see any tags, don't return anything
     if (result.getTargets().size() < 1) {
       return Optional.empty();
     }
