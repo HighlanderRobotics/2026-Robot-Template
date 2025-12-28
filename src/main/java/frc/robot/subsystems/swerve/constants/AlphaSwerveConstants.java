e137eb669fba4d63fcaa93497724cb628853a9af
spellingcat <70864274+spellingcat@users.noreply.github.com>
2025-12-28 13:32:08 -0800

    calib other 2 cameras

--- src/main/java/frc/robot/subsystems/swerve/constants/AlphaSwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/constants/AlphaSwerveConstants.java
@@ -64,13 +64,13 @@ public class AlphaSwerveConstants extends SwerveConstants {
             "Back_Left",
             new Transform3d(
                 new Translation3d(
-                    Units.inchesToMeters(9.859),
+                    Units.inchesToMeters(-9.859),
                     Units.inchesToMeters(9.665),
-                    Units.inchesToMeters(-8.844)),
+                    Units.inchesToMeters(8.844)),
                 new Rotation3d(
                     Units.degreesToRadians(0.0),
-                    Units.degreesToRadians(63.835 + 90),
-                    Units.degreesToRadians(61.875))),
+                    Units.degreesToRadians(-90 + 61.875), // -61.875 - 45 + 90),
+                    Units.degreesToRadians(-90 + 63.835 + 180))),
             BACK_LEFT_CAMERA_MATRIX,
             BACK_LEFT_DIST_COEFFS);
     final CameraConstants backRightCamConstants =
@@ -106,13 +106,13 @@ public class AlphaSwerveConstants extends SwerveConstants {
             "Front_Left",
             new Transform3d(
                 new Translation3d(
-                    Units.inchesToMeters(-9.859),
+                    Units.inchesToMeters(9.859),
                     Units.inchesToMeters(9.665),
-                    Units.inchesToMeters(-8.844)),
+                    Units.inchesToMeters(8.844)),
                 new Rotation3d(
                     Units.degreesToRadians(0.0),
-                    Units.degreesToRadians(63.835),
-                    Units.degreesToRadians(61.875))),
+                    Units.degreesToRadians(-90 + 61.875), // -61.875 - 45 + 90),
+                    Units.degreesToRadians(63.835))),
             FRONT_LEFT_CAMERA_MATRIX,
             FRONT_LEFT_DIST_COEFFS);
 
